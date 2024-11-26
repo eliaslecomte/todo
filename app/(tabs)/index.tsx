303d6d3be0b96f4074a6b3c60af48ad3fce26eb7
Elias Lecomte <elias.lecomte@gmail.com>
2024-11-26 22:39:19 +0100

    feat: add a dummy todo component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, TextInput, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useRef, useState } from 'react';
 
 export default function HomeScreen() {
+  const [todoInput, setTodoInput] = useState('');
+
+  const [todoItems, setTodoItems] = useState<string[]>([]);
+
+  const handleAddTodo = () => {
+    if (todoInput.trim()) { // Prevent adding empty todos
+      setTodoItems([...todoItems, todoInput.trim()]);
+      setTodoInput(''); // Clear input
+    }
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -21,34 +33,11 @@ export default function HomeScreen() {
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <TextInput placeholder='add a todo' value={todoInput} onChangeText={setTodoInput}/>
+        <Button title='Add' onPress={handleAddTodo} />
+        {todoItems.map((todo, index) => (
+          <ThemedText key={index}>{todo}</ThemedText>
+        ))}
       </ThemedView>
     </ParallaxScrollView>
   );
